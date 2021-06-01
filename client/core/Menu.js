35d923ec83344b472da3ed6861a5a7fb89748ed4
waqasshah191 <syedwaqasshah191@gmail.com>
2021-06-01 16:09:09 -0600

    update

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -18,7 +18,7 @@ const Menu = withRouter(({history}) => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="h6" color="inherit">
-        Eco Products Journal
+        Eco 
       </Typography>
       <Link to="/">
         <IconButton aria-label="Home" style={isActive(history, "/")}>
